e4cae6ee2b10785d76c23e3b530eaa4bc786f561
Adam Tjaden <tjadenad@uwec.edu>
2016-10-11 19:22:04 -0500

    Add mock web API

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,6 +2,10 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule }   from '@angular/forms';
 import { RouterModule }  from '@angular/router';
+import { HttpModule }    from '@angular/http';
+
+import { InMemoryWebApiModule } from 'angular-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
 
 import { HeroesComponent }     from './heroes.component';
 import { AppComponent }        from './app.component';
@@ -14,6 +18,8 @@ import { DashboardComponent }  from './dashboard.component';
   imports: [
     BrowserModule,
     FormsModule,
+    HttpModule,
+    InMemoryWebApiModule.forRoot(InMemoryDataService),
     RouterModule.forRoot([
       {
         path: '',
